c244145b11dbd6b3defa38783013f2d5981fcf7d
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-08 14:45:28 +0200

    added html , script and the first prompt for the user

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+const userKm = prompt("Inserisci i kilometri da percorrere")
+const userAge = prompt("Inserisci la tua età")
+
+document.getElementById("userKm").innerHTML = (userKm);
+document.getElementById("userAge").innerHTML = (userAge);
