2d86a2a2dba666d00144822d99357ad8a188bd61
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-08 15:46:21 +0200

    added if statment and  toFixed format for visualising only 2 decimal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 
-const userKm = prompt("Inserisci i kilometri da percorrere")
-const userAge = prompt("Inserisci la tua età")
-
+const userKm = prompt("Inserisci i kilometri da percorrere");
+const userAge = prompt("Inserisci la tua età");
 document.getElementById("userKm").innerHTML = (userKm);
 document.getElementById("userAge").innerHTML = (userAge);
+
+let basePrice = (userKm * 0.276)
+let finalPrice;
+
+document.getElementById("basePrice").innerHTML = (basePrice.toFixed(2));
+
+if (userAge < 18) {
+    finalPrice = basePrice - (basePrice / 100 * 21);
+} else if(userAge > 65){
+    finalPrice = basePrice - (basePrice / 100 * 42) ;
+}else basePrice;
+
+document.getElementById("finalPrice").innerHTML = (finalPrice.toFixed(2));
