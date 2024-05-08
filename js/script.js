9a9a86baf57107e36adfb4041316f2a15464ba83
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-08 15:48:33 +0200

    changed nome of the variable for the discount price

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ document.getElementById("userKm").innerHTML = (userKm);
 document.getElementById("userAge").innerHTML = (userAge);
 
 let basePrice = (userKm * 0.276)
-let finalPrice;
+let discountPrice = 0;
 
 document.getElementById("basePrice").innerHTML = (basePrice.toFixed(2));
 
 if (userAge < 18) {
-    finalPrice = basePrice - (basePrice / 100 * 21);
+    discountPrice = basePrice - (basePrice / 100 * 21);
 } else if(userAge > 65){
-    finalPrice = basePrice - (basePrice / 100 * 42) ;
+    discountPrice = basePrice - (basePrice / 100 * 42) ;
 }else basePrice;
 
-document.getElementById("finalPrice").innerHTML = (finalPrice.toFixed(2));
+document.getElementById("discountPrice").innerHTML = (discountPrice.toFixed(2));
